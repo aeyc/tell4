90e3a7889477df6e6cbe41e8d19c988188370365
aeyc <abegumtascioglu@gmail.com>
2020-09-25 16:04:01 +0300

    Initial commit - login & registration pages design

---
+++ app/src/main/java/com/example/tell4/Main.kt
@@ -0,0 +1,12 @@
+package com.example.tell4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
